527f4cf27ed680beda0b538bc60405df0a7c8670
dbrailov <dbrailov>
2021-02-16 20:16:52 +0200

    Commit

--- src/main/java/com/springredditclone/model/NotificationEmail.java
+++ src/main/java/com/springredditclone/model/NotificationEmail.java
@@ -1,4 +1,15 @@
 package com.springredditclone.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NotificationEmail {
+
+    private String Subject;
+    private String recipient;
+    private String body;
 }
