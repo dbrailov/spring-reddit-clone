f7d8c6cdd396a16f8a39bea5c794f414bb55894a
dbrailov <57748147+dbrailov@users.noreply.github.com>
2021-02-16 19:45:36 +0200

    Add files via upload

---
+++ spring-reddit-clone/src/main/java/com/springredditclone/repository/VerificationTokenRepository.java
@@ -0,0 +1,9 @@
+package com.springredditclone.repository;
+
+import com.springredditclone.model.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+}
